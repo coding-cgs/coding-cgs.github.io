1f8c2db96c0ae109dbb0042e3f9e026659e89b69
chenguosheng <chenguosheng@apexsoft.com.cn>
2022-06-07 14:08:28 +0800

    添加博学字段

--- second_edition/vue.config.js
+++ second_edition/vue.config.js
@@ -1,4 +1,16 @@
 module.exports = {
     lintOnSave:false,
+    css:{
+        loaderOptions:{
+            postcss:{
+                plugins:[
+                    require('postcss-pxtorem')({
+                        rootValue:37.5,
+                        propList:['*']
+                    })
+                ]
+            }
+        }
+    },
     publicPath: process.env.NODE_ENV === 'production' ? './' : '/'
 }
